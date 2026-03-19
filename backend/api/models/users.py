8dfd6a0cfe5afd23e6a52d113180ff791e6e4522
iniestadevmz <iniesta.dev.mz@gmail.com>
2026-03-19 16:34:29 +0200

    Método para obter nome completo

--- backend/api/models/users.py
+++ backend/api/models/users.py
@@ -41,7 +41,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
     
+    def get_full_name(self):
+        full_name = f"{self.first_name} {self.last_name}".strip()
+        return full_name if full_name else self.email
+    
 
-def get_full_name(self):
-    full_name = f"{self.first_name} {self.last_name}".strip()
-    return full_name if full_name else self.email
+
